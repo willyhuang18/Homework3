ff1dc22d0fec1e82e117b0d252fdf0297e715a8d
wiily <willyhuang18@gmail.com>
2021-12-21 21:47:43 -0800

    while loop

--- script.js
+++ script.js
@@ -2,13 +2,38 @@
 var generateBtn = document.querySelector("#generate");
 
 var length = prompt("Please choose length of 8 - 128 character for your password! ");
-while(length < 8 || length > 128 || (isNaN)){
+while(length < 8 || length > 128 || isNaN(length)){
   length = prompt("Please Enter again, you Entered an invalid Character.");
 }
+
 var upperCharacter = "ABCDEFHGIJKLMNOPQRSTUVWXYZ";
 var lowerCharacter = "abcdefghijklmnopqrstuvwxyz";
 var specialCharacter = "!@#$%^&*(){}[]=<>/,.";
 var number = "0123456789";
+
+
+
+while(true){
+  if(upperCharacter){
+    upperCharacter = confirm("Do you want to use UpperCase Character?")
+  }
+  if (lowerCharacter){
+    lowerCharacter = confirm("Do you want to use LowerCase Character?")
+  }
+  if(specialCharacter){
+    specialCharacter = confirm("Do you want to use SpecialCharacter?")
+  }
+  if(number){
+    number = confirm("Do you want to use Number?")
+  }
+  if(!upperCharacter && !lowerCharacter && !specialCharacter && !number){
+    alert("At least one type of character ")
+  }
+  break;
+}
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -20,6 +45,6 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", function (){
+generateBtn.addEventListener("click", () => {
   writePassword();
 });
