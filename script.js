77d6c029fc6757cafc0b4a73bdfdfac02b9032b9
wiily <willyhuang18@gmail.com>
2021-12-21 19:53:54 -0800

    first prompt

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var length = prompt("Please choose length of 8 - 128 character for your password! ");
+while(length < 8 || length > 128 || (isNaN)){
+  length = prompt("Please Enter again, you Entered an invalid Character.");
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
