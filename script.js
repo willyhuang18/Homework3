ce2a49f4fbfaa01b14a7e3b77bbc9a517892c84d
wiily <willyhuang18@gmail.com>
2021-12-21 19:59:12 -0800

    variable

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ var length = prompt("Please choose length of 8 - 128 character for your password
 while(length < 8 || length > 128 || (isNaN)){
   length = prompt("Please Enter again, you Entered an invalid Character.");
 }
+var upperCharacter = "ABCDEFHGIJKLMNOPQRSTUVWXYZ";
+var lowerCharacter = "abcdefghijklmnopqrstuvwxyz";
+var specialCharacter = "!@#$%^&*(){}[]=<>/,.";
+var number = "0123456789";
 
 // Write password to the #password input
 function writePassword() {
